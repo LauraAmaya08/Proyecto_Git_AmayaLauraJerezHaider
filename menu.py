274870bfd9c656d72f60f68eaa3ce040f411aadb
haiderjerez <haiderjerez.16@gmail.com>
2024-07-12 15:25:53 -0500

    feat:

---
+++ menu.py
@@ -0,0 +1,46 @@
+def menu_principal ():
+    print("""
+🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎
+        CITY LOCATION 
+🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎
+    (1)VER CIUDADES 
+    (2)AÑADIR CIUDAD
+    (3)ACTUALIZAR 
+    (4)SALIR 
+""")
+
+def menu_VerCiudad (): 
+    print("""
+🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎
+        CITY LOCATION  
+🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎
+       ¿QUE DESEAS VER?
+    (1)VER TODAS LAS CIUDADES 
+    (2)BUSCAR CIUDAD 
+    (3)SALIR
+""")
+    
+def menu_Buscar ():
+    print("""
+🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎
+        CITY LOCATION  
+🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎
+  ¿COMO LO DESEAS FILTRAR?
+    (1)POR EL NOMBRE 
+    (2)POR EL PAIS
+    (3)POR EL CODIGO POSTAL 
+    (4)SALIR
+""")
+
+def menu_Actualizar ():
+    print("""
+🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎
+        CITY LOCATION  
+🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎🌎
+  ¿QUE DESEAS ACTUALIZAR?
+    (1)NOMBRE DE LA CIUDAD 
+    (2)CODIGO POSTAL
+    (3)POBLACION ESTIMADA
+    (4)PAIS 
+    (5)SALIR
+""")
